7d4bab8b25b32cae8d170c23514c689ffe3bf01f
Patrick Martins <pattrickpx99@gmail.com>
2020-04-25 17:24:21 -0300

    mudança de vértice e exibição de vértices

--- grafo.py
+++ grafo.py
@@ -16,17 +16,25 @@ class grafo:
         for i in range(len(matriz)):
             for j in range(len(matriz[0])):
                 n=0
+                J=0
+                I=0
                 if  self.valido(matriz[i][j]):
                     if i>0 and self.valido(matriz[i-1][j]):
                             n+=1
+                            I-=1
                     if j>0 and self.valido(matriz[i][j-1]):
                             n+=1
+                            J-=1
                     if i<len(matriz)-2 and self.valido(matriz[i+1][j]):
                             n+=1
+                            I+=1
                     if j<len(matriz[0])-2 and self.valido(matriz[i][j+1]):
                             n+=1
-                if n>2:
+                            J+=1
+                if n>2 or I!=0 or J!=0 :
                     self.vertices.append(vertice(i,j))
+    def get_vertices(self):
+        return self.vertices
 
 
                     
